8af74a16ea2f53f93fb4d527ec209a6d262b1b57
Muhammad Jaury <muhjaury@gmail.com>
2025-02-01 22:44:32 +0800

    Fix WrapperRight of Header

--- src/components/Header/_header.tsx
+++ src/components/Header/_header.tsx
@@ -1,6 +1,50 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+  padding: 16px 80px;
   min-height: 80px;
   background: transparent;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const WrapperLeft = styled.div`
+  background: transparent;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const WrapperRight = styled.div`
+  background: transparent;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 24px;
+`;
+
+export const Nav = styled.span`
+  color: white;
+  font-size: 16px;
+  font-weight: 600;
+  cursor: pointer;
+  transition: 0.5s;
+
+  &:hover {
+    color: #d9d9d9;
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  min-height: 50px;
+  display: flex;
+  align-items: center;
+
+  > div {
+    transition: 0.2s;
+
+    &:hover {
+      padding-bottom: 8px;
+    }
+  }
 `;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,26 @@
-import { Wrapper } from "./_header";
+import { Button } from "..";
+import {
+  ButtonWrapper,
+  Nav,
+  Wrapper,
+  WrapperLeft,
+  WrapperRight,
+} from "./_header";
 
 function Header() {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <WrapperLeft></WrapperLeft>
+      <WrapperRight>
+        <Nav>Beranda</Nav>
+        <Nav>Tentang Kami</Nav>
+        <Nav>Program</Nav>
+        <ButtonWrapper>
+          <Button>Hubungi Kami</Button>
+        </ButtonWrapper>
+      </WrapperRight>
+    </Wrapper>
+  );
 }
 
 export default Header;
