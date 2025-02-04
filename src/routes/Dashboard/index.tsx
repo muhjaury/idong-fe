d824055ead3682575ff77aadd17f542971528812
Muhammad Jaury <muhjaury@gmail.com>
2025-02-04 22:14:01 +0800

    Add circle-blue svg

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -2,14 +2,20 @@
 
 import { Button, Content, Section, SvgPreviewer } from "@/components";
 import CoreLayout from "@/layout/CoreLayout";
+import CIRCLE_BLUE from "./../../assets/svg/circle-blue.svg";
 import CIRCLE from "./../../assets/svg/circle.svg";
 
 function Dashboard() {
   return (
     <CoreLayout>
       <Section>
-        <SvgPreviewer src={CIRCLE} alt="circle" top="-32px" right="-200px" />
-        <SvgPreviewer src={CIRCLE} alt="circle" top="80px" right="0" />
+        <SvgPreviewer
+          src={CIRCLE_BLUE}
+          alt="circle"
+          top="-32px"
+          right="-200px"
+        />
+        <SvgPreviewer src={CIRCLE_BLUE} alt="circle" top="80px" right="0" />
       </Section>
       <Section type="secondary">
         <SvgPreviewer src={CIRCLE} alt="circle" top="200" right="0" />
