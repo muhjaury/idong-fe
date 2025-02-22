7e575eb44f29710a9b9a2c65673c591e9275f812
Muhammad Jaury <muhjaury@gmail.com>
2025-02-22 18:45:24 +0700

    Fix SideBar css

--- src/components/HeaderUser/index.tsx
+++ src/components/HeaderUser/index.tsx
@@ -1,11 +1,19 @@
+import { useRouter } from "next/navigation";
 import { Button } from "..";
 import { Title, Wrapper } from "./_headerUser";
+import { urls } from "@/constant/path";
 
 function HeaderUser() {
+  const router = useRouter();
+
+  const handleLogoutClick = () => {
+    router.push(urls.HOME);
+  };
+
   return (
     <Wrapper>
       <Title>SMKS Karya Teknik Watansoppeng</Title>
-      <Button>Logout</Button>
+      <Button onClick={handleLogoutClick}>Logout</Button>
     </Wrapper>
   );
 }
