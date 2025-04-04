56a601c63c8005842078823a34ea0fef8dc10591
idonG <muh.faridd157@gmail.com>
2025-04-04 14:37:06 +0800

    Update Sejarah

--- src/routes/Sejarah/index.tsx
+++ src/routes/Sejarah/index.tsx
@@ -16,7 +16,14 @@ function Sejarah() {
   return (
     <CoreLayout>
       <PageTitle title="Sejarah" breadcrumb={breadcrumb} />
-      <Section type="secondary"></Section>
+      <Section type="secondary">SMKS Karya Teknik Watansoppeng didirikan tanggal 5 juli 1969 
+      di atas lahan seluas 12.239 m 2 dengan Nomor SK pendirian 120/UK.3/1969 dengan 
+      nama SMKS Karya Teknik Watansoppeng. Sekolah ini merupakan sekolah menengah kejuruan 
+      tertua di kabupaten Soppeng yang sekarang ini bernama SMKS Karya Teknik Watansoppeng, 
+      NPSS 40304501, berakreditasi A. SMKS Karya Teknik Watansoppen berlokasi di jalan tekukur No. 
+      1 Sinjai kelurahan biringere, kecamatan sinjai utara yang dikelilingi oleh 
+      empat jalan yaitu di sebelah Barat Jalan Tekukur, di sebelah utara Jalan Teuku Umar, 
+      sebelah timur Jalan Wolter Monginsidi, dan di sebelah selatan Jalan Tamrin.</Section>
     </CoreLayout>
   );
 }
