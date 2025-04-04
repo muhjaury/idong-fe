2c485a4ccdeaa34b293508a8a80111a71322abc9
idonG <muh.faridd157@gmail.com>
2025-04-04 15:11:02 +0800

    Update SiswaPage and add TambahSiswa in ModalContent component

---
+++ src/components/ModalContent/TambahSiswa/index.tsx
@@ -0,0 +1,144 @@
+import { Button, FieldWrapper, InputText } from "@/components";
+import { decryptData } from "@/utils/dataManipulation";
+import { useEffect, useState } from "react";
+import { ButtonWrapper, PromptText, Title, Wrapper } from "./_tambahSiswa";
+import { saveData } from "./network";
+
+function TambahSiswa(props: any) {
+  const [id, setId] = useState("");
+  const [kelas, setKelas] = useState("");
+  const [nis, setNis] = useState("");
+  const [nama, setNama] = useState("");
+  const [jenisKelamin, setJenisKelamin] = useState("");
+  const [alamat, setAlamat] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  //Prefill
+  useEffect(() => {
+    const data = props?.data;
+    if (
+      data?.kelas &&
+      data?.nis &&
+      data?.nama &&
+      data?.jenisKelamin &&
+      data?.alamat
+    ) {
+      if (data?.id) {
+        setId(data?.id);
+      }
+      setKelas(decryptData(data.kelas));
+      setNis(decryptData(data.nis));
+      setNama(decryptData(data.nama));
+      setJenisKelamin(decryptData(data.jenisKelamin));
+      setAlamat(decryptData(data.alamat));
+    }
+  }, [props?.data]);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (success) {
+      setTimeout(() => {
+        setSuccess("");
+      }, 3000);
+    }
+  }, [success]);
+
+  const handleKelasChange = (e: any) => {
+    setKelas(e.target.value);
+  };
+
+  const handleNisChange = (e: any) => {
+    setNis(e.target.value);
+  };
+
+  const handleNamaChange = (e: any) => {
+    setNama(e.target.value);
+  };
+
+  const handleJenisKelaminChange = (e: any) => {
+    setJenisKelamin(e.target.value);
+  };
+
+  const handleAlamatChange = (e: any) => {
+    setAlamat(e.target.value);
+  };
+
+  const handleAddClick = () => {
+    if (kelas && nis && nama && jenisKelamin && alamat) {
+      const data = { id, kelas, nis, nama, jenisKelamin, alamat };
+      const func = {
+        setId,
+        setKelas,
+        setNis,
+        setNama,
+        setJenisKelamin,
+        setAlamat,
+        setError,
+        setSuccess,
+      };
+      saveData(func, data);
+    } else {
+      setError("Semua data harus diisi");
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Title>{id ? "Ubah Data" : "Tambah Data"}</Title>
+      <FieldWrapper>
+        <InputText
+          label="Kelas"
+          value={kelas}
+          onChange={(e) => handleKelasChange(e)}
+        />
+      </FieldWrapper>
+      <FieldWrapper>
+        <InputText
+          label="NIS"
+          value={nis}
+          onChange={(e) => handleNisChange(e)}
+        />
+      </FieldWrapper>
+      <FieldWrapper>
+        <InputText
+          label="Nama"
+          value={nama}
+          onChange={(e) => handleNamaChange(e)}
+        />
+      </FieldWrapper>
+      <FieldWrapper>
+        <InputText
+          label="Jenis Kelamin"
+          value={jenisKelamin}
+          onChange={(e) => handleJenisKelaminChange(e)}
+        />
+      </FieldWrapper>
+      <FieldWrapper>
+        <InputText
+          label="Alamat"
+          value={alamat}
+          onChange={(e) => handleAlamatChange(e)}
+        />
+      </FieldWrapper>
+      <PromptText iserror={error}>{error || success}</PromptText>
+      <ButtonWrapper>
+        <Button removeshadow="Y" onClick={() => props.onClose()}>
+          Batal
+        </Button>
+        <Button removeshadow="Y" onClick={() => handleAddClick()}>
+          Simpan
+        </Button>
+      </ButtonWrapper>
+    </Wrapper>
+  );
+}
+
+export default TambahSiswa;
--- src/routes/Dashboard/Pages/SiswaPage/index.tsx
+++ src/routes/Dashboard/Pages/SiswaPage/index.tsx
@@ -1,5 +1,104 @@
+import { Button, InputText, Modal } from "@/components";
+import { TambahSiswa } from "@/components/ModalContent";
+import { useEffect, useState } from "react";
+import DataTable, { TableColumn } from "react-data-table-component";
+import { Wrapper, WrapperContent, WrapperSearch } from "./_siswaPage";
+import { fetch } from "./network";
+
 function SiswaPage() {
-  return <>Siswa Page</>;
+  const [rawData, setRawData] = useState<any[]>([]);
+  const [data, setData] = useState(rawData);
+  const [search, setSearch] = useState("");
+  const [displayModal, setDisplayModal] = useState(false);
+  const [modalData, setModalData] = useState({});
+
+  useEffect(() => {
+    if (rawData.length === 0) {
+      const func = { setDisplayModal, setModalData, setRawData };
+      fetch(func);
+    }
+  }, []);
+
+  useEffect(() => {
+    setData(rawData);
+  }, [rawData]);
+
+  const columns: TableColumn<any>[] = [
+    { name: "Nomor", selector: (row) => row.nomor, sortable: true },
+    {
+      name: "Kelas",
+      selector: (row) => row.kelas,
+      sortable: true,
+    },
+    {
+      name: "NIS",
+      selector: (row) => row.nis,
+      sortable: true,
+    },
+    {
+      name: "Nama",
+      selector: (row) => row.nama,
+      sortable: true,
+    },
+    {
+      name: "Jenis Kelamin",
+      selector: (row) => row.jenisKelamin,
+      sortable: true,
+    },
+    {
+      name: "Alamat",
+      selector: (row) => row.alamat,
+      sortable: true,
+    },
+    { name: "Aksi", selector: (row) => row.aksi, sortable: true },
+  ];
+
+  const handleSearch = (e: any) => {
+    const value = e.target.value;
+    setSearch(value);
+    const filterData = rawData.filter(
+      (row) =>
+        row?.nis?.toLowerCase().includes(value.toLowerCase()) ||
+        row?.nama?.toLowerCase().includes(value.toLowerCase())
+    );
+    setData(filterData);
+  };
+
+  return (
+    <>
+      <Wrapper>
+        <Button onClick={() => setDisplayModal(true)}>Tambah Siswa</Button>
+        <WrapperContent>
+          <WrapperSearch>
+            <InputText
+              placeholder="Pencarian"
+              onChange={(e: any) => handleSearch(e)}
+              value={search}
+            />
+          </WrapperSearch>
+          <DataTable columns={columns} data={data} pagination></DataTable>
+        </WrapperContent>
+      </Wrapper>
+      <Modal
+        type="secondary"
+        display={displayModal}
+        onClose={() => {
+          setDisplayModal(false);
+          setModalData({});
+          const func = { setDisplayModal, setModalData, setRawData };
+          fetch(func);
+        }}
+      >
+        <TambahSiswa
+          data={modalData}
+          onClose={() => {
+            setDisplayModal(false);
+            setModalData({});
+          }}
+        />
+      </Modal>
+    </>
+  );
 }
 
 export default SiswaPage;
