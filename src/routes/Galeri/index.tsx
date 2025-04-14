7308ee71ae1630f8914a3eff04d0652558bb20d4
Muhammad Jaury <muhjaury@gmail.com>
2025-04-14 23:12:32 +0800

    Update Galeri

--- src/routes/Galeri/index.tsx
+++ src/routes/Galeri/index.tsx
@@ -1,22 +1,48 @@
-import { PageTitle, Section } from "@/components";
+import { Card, PageTitle, Section } from "@/components";
 import { Breadcrumb } from "@/constant/breadcrumb";
 import CoreLayout from "@/layout/CoreLayout";
 import { useEffect, useState } from "react";
+import { fetch } from "./network";
+import { CardDesc, CardTitle, CardWrapper } from "./_galeri";
 
 function Galeri() {
   const [breadcrumb, setBreadcrumb] = useState([
     Breadcrumb.home,
     Breadcrumb.galeri,
   ]);
+  const [list, setList] = useState([]);
 
   useEffect(() => {
     setBreadcrumb([Breadcrumb.home, Breadcrumb.galeri]);
   }, []);
 
+  useEffect(() => {
+    const func = { setList };
+    fetch(func);
+  }, []);
+
   return (
     <CoreLayout>
       <PageTitle title="Galeri" breadcrumb={breadcrumb} />
-      <Section type="secondary"></Section>
+      <Section type="secondary">
+        <CardWrapper>
+          {list.length > 0 &&
+            list.map((item: any, index: number) => {
+              return (
+                <Card
+                  key={index}
+                  image={item.foto}
+                  contentBottom={
+                    <>
+                      <CardTitle>{item.judul}</CardTitle>
+                      <CardDesc>{item.deskripsi}</CardDesc>
+                    </>
+                  }
+                />
+              );
+            })}
+        </CardWrapper>
+      </Section>
     </CoreLayout>
   );
 }
