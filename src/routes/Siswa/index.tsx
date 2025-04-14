28d1469a7eeb3fc3bed2cd25dc92e32583d4240b
Muhammad Jaury <muhjaury@gmail.com>
2025-04-14 23:25:00 +0800

    Update Siswa

--- src/routes/Siswa/index.tsx
+++ src/routes/Siswa/index.tsx
@@ -1,22 +1,91 @@
-import { PageTitle, Section } from "@/components";
+import { InputText, PageTitle, Section } from "@/components";
 import { Breadcrumb } from "@/constant/breadcrumb";
 import CoreLayout from "@/layout/CoreLayout";
 import { useEffect, useState } from "react";
+import DataTable, { TableColumn } from "react-data-table-component";
+import { Wrapper, WrapperContent, WrapperSearch } from "./_siswa";
+import { fetch } from "./network";
 
 function Siswa() {
   const [breadcrumb, setBreadcrumb] = useState([
     Breadcrumb.home,
     Breadcrumb.siswa,
   ]);
+  const [rawData, setRawData] = useState<any[]>([]);
+  const [data, setData] = useState(rawData);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     setBreadcrumb([Breadcrumb.home, Breadcrumb.siswa]);
   }, []);
 
+  useEffect(() => {
+    if (rawData.length === 0) {
+      const func = { setRawData };
+      fetch(func);
+    }
+  }, []);
+
+  useEffect(() => {
+    setData(rawData);
+  }, [rawData]);
+
+  const columns: TableColumn<any>[] = [
+    { name: "Nomor", selector: (row) => row.nomor, sortable: true },
+    {
+      name: "Kelas",
+      selector: (row) => row.kelas,
+      sortable: true,
+    },
+    {
+      name: "NIS",
+      selector: (row) => row.nis,
+      sortable: true,
+    },
+    {
+      name: "Nama",
+      selector: (row) => row.nama,
+      sortable: true,
+    },
+    {
+      name: "Jenis Kelamin",
+      selector: (row) => row.jenisKelamin,
+      sortable: true,
+    },
+    {
+      name: "Alamat",
+      selector: (row) => row.alamat,
+      sortable: true,
+    },
+  ];
+
+  const handleSearch = (e: any) => {
+    const value = e.target.value;
+    setSearch(value);
+    const filterData = rawData.filter(
+      (row) =>
+        row?.nis?.toLowerCase().includes(value.toLowerCase()) ||
+        row?.nama?.toLowerCase().includes(value.toLowerCase())
+    );
+    setData(filterData);
+  };
   return (
     <CoreLayout>
       <PageTitle title="Siswa" breadcrumb={breadcrumb} />
-      <Section type="secondary"></Section>
+      <Section type="secondary">
+        <Wrapper>
+          <WrapperContent>
+            <WrapperSearch>
+              <InputText
+                placeholder="Pencarian"
+                onChange={(e: any) => handleSearch(e)}
+                value={search}
+              />
+            </WrapperSearch>
+            <DataTable columns={columns} data={data} pagination></DataTable>
+          </WrapperContent>
+        </Wrapper>
+      </Section>
     </CoreLayout>
   );
 }
---
+++ src/routes/Siswa/network.tsx
@@ -0,0 +1,25 @@
+import { API_FETCH_SISWA } from "@/constant/api";
+import { decryptData } from "@/utils/dataManipulation";
+import request from "@/utils/request";
+
+export const fetch = (func: any) => {
+  const { setRawData } = func;
+  request(API_FETCH_SISWA, "GET", null, null)
+    .then((res) => {
+      if (res?.status.toLowerCase() === "success") {
+        let result = res?.data;
+        result = result.map((item: any, index: number) => {
+          return {
+            nomor: index + 1,
+            kelas: decryptData(item.kelas),
+            nis: decryptData(item.nis),
+            nama: decryptData(item.nama),
+            jenisKelamin: decryptData(item.jenisKelamin),
+            alamat: decryptData(item.alamat),
+          };
+        });
+        setRawData(result);
+      }
+    })
+    .catch((e) => console.log(e));
+};
