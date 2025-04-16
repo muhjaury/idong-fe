14b288a90f210ffb2a3224f4f93ab9d2a106564c
Muhammad Jaury <muhjaury@gmail.com>
2025-04-17 04:42:14 +0800

    Update ATPH

--- src/routes/ATPH/index.tsx
+++ src/routes/ATPH/index.tsx
@@ -1,13 +1,21 @@
-import { PageTitle, Section } from "@/components";
+import { DisplayFile, PageTitle, Section } from "@/components";
 import { Breadcrumb } from "@/constant/breadcrumb";
 import CoreLayout from "@/layout/CoreLayout";
 import { useEffect, useState } from "react";
+import { Wrapper } from "./_ATPH";
+import { fetch } from "./network";
 
 function ATPH() {
   const [breadcrumb, setBreadcrumb] = useState([
     Breadcrumb.home,
     Breadcrumb.atph,
   ]);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const func = { setList };
+    fetch(func);
+  }, []);
 
   useEffect(() => {
     setBreadcrumb([Breadcrumb.home, Breadcrumb.atph]);
@@ -16,7 +24,20 @@ function ATPH() {
   return (
     <CoreLayout>
       <PageTitle title="ATPH" breadcrumb={breadcrumb} />
-      <Section type="secondary"></Section>
+      <Section type="secondary">
+        <Wrapper>
+          {list.length > 0 &&
+            list.map((item: any, index: number) => {
+              return (
+                <DisplayFile
+                  key={index}
+                  base64={item.file}
+                  label={item.deskripsi}
+                />
+              );
+            })}
+        </Wrapper>
+      </Section>
     </CoreLayout>
   );
 }
