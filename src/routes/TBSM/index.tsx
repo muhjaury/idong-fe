329c4f5c8f7d4c0fd02a241c4751bff853c0d901
Muhammad Jaury <muhjaury@gmail.com>
2025-04-17 04:37:20 +0800

    Update TBSM

--- src/routes/TBSM/index.tsx
+++ src/routes/TBSM/index.tsx
@@ -1,13 +1,21 @@
-import { PageTitle, Section } from "@/components";
+import { DisplayFile, PageTitle, Section } from "@/components";
 import { Breadcrumb } from "@/constant/breadcrumb";
 import CoreLayout from "@/layout/CoreLayout";
 import { useEffect, useState } from "react";
+import { Wrapper } from "./_TBSM";
+import { fetch } from "./network";
 
 function TBSM() {
   const [breadcrumb, setBreadcrumb] = useState([
     Breadcrumb.home,
     Breadcrumb.tbsm,
   ]);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const func = { setList };
+    fetch(func);
+  }, []);
 
   useEffect(() => {
     setBreadcrumb([Breadcrumb.home, Breadcrumb.tbsm]);
@@ -16,7 +24,20 @@ function TBSM() {
   return (
     <CoreLayout>
       <PageTitle title="TBSM" breadcrumb={breadcrumb} />
-      <Section type="secondary"></Section>
+      <Section type="secondary">
+        <Wrapper>
+          {list.length > 0 &&
+            list.map((item: any, index: number) => {
+              return (
+                <DisplayFile
+                  key={index}
+                  base64={item.file}
+                  label={item.deskripsi}
+                />
+              );
+            })}
+        </Wrapper>
+      </Section>
     </CoreLayout>
   );
 }
