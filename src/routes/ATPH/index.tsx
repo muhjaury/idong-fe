79851c02dbdf1ca6c164ed890d1d829e1bdc60b8
Muhammad Jaury <muhjaury@gmail.com>
2025-05-11 13:01:44 +0700

    Update ATPH & DPIB

--- src/routes/ATPH/index.tsx
+++ src/routes/ATPH/index.tsx
@@ -2,7 +2,7 @@ import { DisplayFile, PageTitle, Section } from "@/components";
 import { Breadcrumb } from "@/constant/breadcrumb";
 import CoreLayout from "@/layout/CoreLayout";
 import { useEffect, useState } from "react";
-import { Wrapper } from "./_ATPH";
+import { Modul, Ol, Paragraph, Wrapper } from "./_ATPH";
 import { fetch } from "./network";
 
 function ATPH() {
@@ -26,16 +26,53 @@ function ATPH() {
       <PageTitle title="ATPH" breadcrumb={breadcrumb} />
       <Section type="secondary">
         <Wrapper>
-          {list.length > 0 &&
-            list.map((item: any, index: number) => {
-              return (
-                <DisplayFile
-                  key={index}
-                  base64={item.file}
-                  label={item.deskripsi}
-                />
-              );
-            })}
+          <Paragraph>
+            &nbsp;&nbsp;&nbsp;ATPH adalah singkatan dari Agribisnis Tanaman
+            Pangan dan Hortikultura. Jurusan ini mempelajari tentang budidaya
+            tanaman pangan (seperti padi, jagung, kedelai) dan tanaman
+            hortikultura (seperti sayuran, buah-buahan, dan tanaman hias), serta
+            aspek bisnis dan pemasaran hasil pertanian.
+          </Paragraph>
+          <Paragraph>
+            &nbsp;&nbsp;&nbsp;Tujuan utama jurusan ini adalah untuk membekali
+            siswa dengan keterampilan teknis dan manajerial agar bisa bekerja di
+            sektor pertanian modern, baik sebagai pekerja profesional maupun
+            sebagai wirausahawan. Lulusan a ATPH bisa bekerja di berbagai
+            bidang, petani modern atau wirausahawan pertanian, teknisi pertanian
+            (di perusahaan atau dinas pertanian), pengusaha bibit tanaman atau
+            pupuk, pengelola greenhouse atau kebun hortikultura, karyawan di
+            perusahaan benih, pupuk, atau pestisida, staf agribisnis atau
+            distributor hasil pertanian. Beberapa mata pelajaran kejuruan dalam
+            ATPH:
+          </Paragraph>
+          <br />
+          <Ol type="a">
+            <li>Dasar-Dasar Agribisnis</li>
+            <li>Budidaya Tanaman Pangan</li>
+            <li>BudidayaTanaman Hortikultura</li>
+            <li>Alat dan Mesin Pertanian (Alsintan)</li>
+            <li>Pengolahan Hasil Pertanian</li>
+            <li>Pengendalian Hama dan Penyakit</li>
+            <li>Manajemen Usaha Tani</li>
+            <li>Pemasaran dan Kewirausahaan Pertanian</li>
+            <li>Teknik Irigasi dan Pemupukan</li>
+            <li>Teknologi Tepat Guna di Bidang Pertanian</li>
+          </Ol>
+          <br />
+          {list.length > 0 && (
+            <>
+              <Modul>Modul Pembelajaran</Modul>
+              {list.map((item: any, index: number) => {
+                return (
+                  <DisplayFile
+                    key={index}
+                    base64={item.file}
+                    label={item.deskripsi}
+                  />
+                );
+              })}
+            </>
+          )}
         </Wrapper>
       </Section>
     </CoreLayout>
--- src/routes/DPIB/index.tsx
+++ src/routes/DPIB/index.tsx
@@ -2,7 +2,7 @@ import { DisplayFile, PageTitle, Section } from "@/components";
 import { Breadcrumb } from "@/constant/breadcrumb";
 import CoreLayout from "@/layout/CoreLayout";
 import { useEffect, useState } from "react";
-import { Wrapper } from "./_DPIB";
+import { Li, Modul, Ol, Paragraph, Wrapper } from "./_DPIB";
 import { fetch } from "./network";
 
 function DPIB() {
@@ -26,16 +26,93 @@ function DPIB() {
       <PageTitle title="DPIB" breadcrumb={breadcrumb} />
       <Section type="secondary">
         <Wrapper>
-          {list.length > 0 &&
-            list.map((item: any, index: number) => {
-              return (
-                <DisplayFile
-                  key={index}
-                  base64={item.file}
-                  label={item.deskripsi}
-                />
-              );
-            })}
+          <Paragraph>
+            &nbsp;&nbsp;&nbsp;Program keahlian Desain Pemodelan dan Informasi
+            banguna memiliki kurikulum yang dirancang khusus untuk mempersiapkan
+            siswa dalam berbagai aspek desain, pemodelan, dan informasi
+            bangunan. Mata pelajaran yang dipelajari dalam program keahlian ini
+            mencakup berbagai keterampilan yang diperlukan untuk sukses dalam
+            industri konstruksi dan arsitektur. Berikut adalah beberapa mata
+            pelajaran yang umumnya diajarkan dalam program keahlian ini:
+          </Paragraph>
+          <br />
+          <Ol type="a">
+            <Li>Desain Arsitektur</Li>
+            <span>
+              Mata pelajaran ini mengajarkan dasar-dasar desain arsitektur,
+              termasuk prinsip-prinsip desain, estetika, dan fungsionalitas
+              bangunan.
+            </span>
+            <Li>Pemodelan 3D</Li>
+            <span>
+              Siswa belajar tentang pemodelan tiga dimensi menggunakan perangkat
+              lunak seperti AutoCAD, SketchUp, atau perangkat lunak pemodelan
+              lainnya.
+            </span>
+            <Li>Pemodelan Informasi Bangunan (BIM)</Li>
+            <span>
+              Siswa memahami konsep dan praktik BIM, termasuk bagaimana
+              mengintegrasikan informasi bangunan ke dalam proyek konstruksi.
+            </span>
+            <Li>Teknologi Konstruksi</Li>
+            <span>
+              Mata pelajaran ini mencakup pemahaman tentang teknologi
+              konstruksi, bahan bangunan, dan metode konstruksi yang berbeda.
+            </span>
+            <Li>Dokumentasi Teknis</Li>
+            <span>
+              Siswa belajar tentang pembuatan dokumen teknis yang diperlukan
+              dalam proyek konstruksi, seperti gambar teknis, spesifikasi, dan
+              perencanaan proyek.
+            </span>
+            <Li>Manajemen Proyek Konstruksi</Li>
+            <span>
+              Mata pelajaran ini mengajarkan prinsip-prinsip dasar manajemen
+              proyek konstruksi, termasuk perencanaan, pengendalian biaya, dan
+              jadwal proyek.
+            </span>
+            <Li>Peraturan Bangunan</Li>
+            <span>
+              Siswa memahami peraturan dan standar bangunan yang berlaku, serta
+              peran penting kepatuhan terhadap peraturan dalam proyek
+              konstruksi.
+            </span>
+            <Li>Computer Aided Design (CAD)</Li>
+            <span>
+              Siswa memperoleh keterampilan dalam penggunaan perangkat lunak CAD
+              untuk membuat gambar teknis dan perencanaan desain.
+            </span>
+            <Li>Keterampilan Komunikasi</Li>
+            <span>
+              Pentingnya komunikasi dalam industri ini diakui, sehingga siswa
+              juga diajarkan keterampilan komunikasi baik lisan maupun tertulis.
+            </span>
+            <Li>Praktik Kerja</Li>
+            <span>
+              Sebagian besar program SMK melibatkan praktik kerja di lapangan,
+              di mana siswa dapat mengaplikasikan pengetahuan dan keterampilan
+              yang mereka pelajari dalam situasi nyata di tempat kerja. Mata
+              pelajaran ini membantu siswa mengembangkan kemampuan yang luas
+              dalam desain, pemodelan, dan manajemen informasi bangunan,
+              mempersiapkan mereka untuk berbagai karier yang terkait dengan
+              industri konstruksi dan arsitektur setelah lulus.
+            </span>
+          </Ol>
+          <br />
+          {list.length > 0 && (
+            <>
+              <Modul>Modul Pembelajaran</Modul>
+              {list.map((item: any, index: number) => {
+                return (
+                  <DisplayFile
+                    key={index}
+                    base64={item.file}
+                    label={item.deskripsi}
+                  />
+                );
+              })}
+            </>
+          )}
         </Wrapper>
       </Section>
     </CoreLayout>
