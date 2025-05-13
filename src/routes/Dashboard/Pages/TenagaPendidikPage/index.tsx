97d9b4580ea92c83f8d45901624106a1dbb1389c
Muhammad Jaury <muhjaury@gmail.com>
2025-05-13 21:14:32 +0700

    Setup loading in TenagaPendidikPage

--- src/routes/Dashboard/Pages/TenagaPendidikPage/index.tsx
+++ src/routes/Dashboard/Pages/TenagaPendidikPage/index.tsx
@@ -1,5 +1,6 @@
 import { Button, InputText, Modal } from "@/components";
 import { TambahTenagaPendidik } from "@/components/ModalContent";
+import { useWidget } from "@/context";
 import { useEffect, useState } from "react";
 import DataTable, { TableColumn } from "react-data-table-component";
 import { Wrapper, WrapperContent, WrapperSearch } from "./_tenagaPendidikPage";
@@ -12,9 +13,12 @@ function TenagaPendidikPage() {
   const [displayModal, setDisplayModal] = useState(false);
   const [modalData, setModalData] = useState({});
 
+  const { setLoading } = useWidget();
+
   useEffect(() => {
     if (rawData.length === 0) {
-      const func = { setDisplayModal, setModalData, setRawData };
+      setLoading(true);
+      const func = { setDisplayModal, setModalData, setRawData, setLoading };
       fetch(func);
     }
   }, []);
@@ -75,17 +79,33 @@ function TenagaPendidikPage() {
         type="secondary"
         display={displayModal}
         onClose={() => {
+          setLoading(true);
+
           setDisplayModal(false);
           setModalData({});
-          const func = { setDisplayModal, setModalData, setRawData };
+          const func = {
+            setDisplayModal,
+            setModalData,
+            setRawData,
+            setLoading,
+          };
           fetch(func);
         }}
       >
         <TambahTenagaPendidik
           data={modalData}
           onClose={() => {
+            setLoading(true);
+
             setDisplayModal(false);
             setModalData({});
+            const func = {
+              setDisplayModal,
+              setModalData,
+              setRawData,
+              setLoading,
+            };
+            fetch(func);
           }}
         />
       </Modal>
