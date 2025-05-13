8d8afa31a4e93496c11aef2dc25c0ae1ec47cb6d
Muhammad Jaury <muhjaury@gmail.com>
2025-05-13 23:11:10 +0700

    Setup loading in TKJPage

--- src/routes/Dashboard/Pages/TKJPage/index.tsx
+++ src/routes/Dashboard/Pages/TKJPage/index.tsx
@@ -1,5 +1,6 @@
 import { Button, InputText, Modal } from "@/components";
 import { TambahTKJ } from "@/components/ModalContent";
+import { useWidget } from "@/context";
 import { useEffect, useState } from "react";
 import DataTable, { TableColumn } from "react-data-table-component";
 import { Wrapper, WrapperContent, WrapperSearch } from "./_TKJPage";
@@ -12,9 +13,13 @@ function TKJPage() {
   const [displayModal, setDisplayModal] = useState(false);
   const [modalData, setModalData] = useState({});
 
+  const { setLoading } = useWidget();
+
   useEffect(() => {
     if (rawData.length === 0) {
-      const func = { setDisplayModal, setModalData, setRawData };
+      setLoading(true);
+
+      const func = { setDisplayModal, setModalData, setRawData, setLoading };
       fetch(func);
     }
   }, []);
@@ -66,17 +71,33 @@ function TKJPage() {
         type="secondary"
         display={displayModal}
         onClose={() => {
+          setLoading(true);
+
           setDisplayModal(false);
           setModalData({});
-          const func = { setDisplayModal, setModalData, setRawData };
+          const func = {
+            setDisplayModal,
+            setModalData,
+            setRawData,
+            setLoading,
+          };
           fetch(func);
         }}
       >
         <TambahTKJ
           data={modalData}
           onClose={() => {
+            setLoading(true);
+
             setDisplayModal(false);
             setModalData({});
+            const func = {
+              setDisplayModal,
+              setModalData,
+              setRawData,
+              setLoading,
+            };
+            fetch(func);
           }}
         />
       </Modal>
